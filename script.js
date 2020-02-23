f301ea412c99cc85793f667860b9cfba4f40afaf
Vitaly <deosfen@gmail.com>
2020-02-23 20:07:23 +1000

    first commit"

---
+++ script.js
@@ -0,0 +1,23 @@
+
+var LeftBorderWidth = 1;
+let Second = 2;
+const pi=3.14;
+
+console.log(LeftBorderWidth)
+
+let Person = {
+    name: "Jon",
+    age: 25,
+    isMarried: false
+};
+
+console.log(Person.name)
+
+//let answer = confirm("Are you sure?")
+//console.log(answer)
+
+let answer = prompt("Are you lefal age>?", "Yes")
+console.log(typeof(answer))
+
+
+
